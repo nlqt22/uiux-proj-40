336fe740b1936d8cf8ed060ed78288835de2177a
Tuan Nguyen Quang <80348326+pinkieyun@users.noreply.github.com>
2023-07-16 13:16:35 +0700

    update

--- src/pages/member/MemberList.jsx
+++ src/pages/member/MemberList.jsx
@@ -1,6 +1,27 @@
 import Card from "@/components/ui/Card";
+import Button from "@/components/ui/Button";
+import Icon from "@/components/ui/Icon";
+
+import { useNavigate } from "react-router-dom";
 
 const MemberList = ({members}) => {
+    const navigate = useNavigate();
+    const actions = [
+        {
+            name: "edit",
+            icon: "heroicons:pencil-square",
+            doit: () => {
+                return null;
+            },
+        },
+        {
+            name: "delete",
+            icon: "heroicons-outline:trash",
+            doit: (id) => {
+                return null;
+            },
+        },
+    ];
 
     return (
         <>
@@ -26,6 +47,9 @@ const MemberList = ({members}) => {
                                 <th scope="col" className=" table-th">
                                     Date of birth
                                 </th>
+                                <th scope="col" className=" table-th">
+                                    Action
+                                </th>
                             </tr>
                         </thead>
 
@@ -44,6 +68,30 @@ const MemberList = ({members}) => {
                                         </td>
                                         <td className="table-td">
                                             { member.dob }
+                                        </td>
+                                        <td className="table-td">
+                                            <div className="divide-y divide-slate-100 dark:divide-slate-800">
+                                                <div className="flex space-x-2 items-center rtl:space-x-reverse">
+                                                    {actions.map((item, i) => (
+                                                        <div
+                                                            key={i}
+                                                            onClick={() => item.doit()}
+                                                            className={`
+                                                                ${
+                                                                    item.icon === "heroicons-outline:trash"
+                                                                    ? "bg-danger-500 text-danger-500 bg-opacity-30 hover:bg-opacity-100 hover:text-white"
+                                                                    : "hover:bg-slate-900 hover:text-white dark:hover:bg-slate-600 dark:hover:bg-opacity-50"
+                                                                }
+                                                                border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm  last:mb-0 cursor-pointer 
+                                                                flex  space-x-2 items-center                                                               `}
+                                                        >
+                                                            <span className="text-base">
+                                                                <Icon icon={item.icon} />
+                                                            </span>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 );
--- src/pages/member/index.jsx
+++ src/pages/member/index.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import useWidth from "@/hooks/useWidth";
 import MemberList from "./MemberList";
-
+import Button from "@/components/ui/Button";
 import TableLoading from "@/components/skeleton/Table";
 import { fetchMembers } from "./store";
+import { ToastContainer } from "react-toastify";
 
 
 const MemberListPage = () =>  {
     const { members, status, error } = useSelector((state) => state.members);
+    const { width, breakpoints } = useWidth();
 
     const dispatch = useDispatch();
 
@@ -27,6 +29,25 @@ const MemberListPage = () =>  {
     if(status === "succeeded") {
         return (
             <div>
+                <ToastContainer />
+                <div className="flex flex-wrap justify-between items-center mb-4">
+                    <h4 className="font-medium lg:text-2xl text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4">
+                        Members
+                    </h4>
+                    <div
+                        className={`${
+                            width < breakpoints.md ? "space-x-rb" : ""
+                        } 
+                            md:flex md:space-x-4 md:justify-end items-center rtl:space-x-reverse`}
+                    >
+                        <Button
+                            icon="heroicons-outline:plus"
+                            text="Add Project"
+                            className="btn-dark dark:bg-slate-800  h-min text-sm font-normal"
+                            iconClass=" text-lg"
+                        />
+                    </div>
+                </div>
                 <MemberList members={ members } />
             </div>
         );
